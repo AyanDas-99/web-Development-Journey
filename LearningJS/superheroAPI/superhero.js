e7cf7d539b04524de2f704afcf6db66f168d67be
Ayan Das <ayandas9531@gmail.com>
2022-09-28 22:58:54 +0530

    understanding superhero api

--- LearningJS/superheroAPI/superhero.js
+++ LearningJS/superheroAPI/superhero.js
@@ -1,3 +1,10 @@
+// document.querySelector('body').style.display = 'none'
+
+// userName = "Your Identity";
+// if(userName.toUpperCase() != 'CHIEF') {
+//     document.querySelector('body').style.display = 'block'
+// }
+
 const base = 'http://gateway.marvel.com/v1/public/'
 const apikey = 'apikey=59a1337b17560afe03d8bba564fffb37'
 const hash = 'hash=d5accdcd6879c0baf56eda51a18e261d'
@@ -15,7 +22,7 @@ const getSuperhero = (name) => {
             const file = (json.data.results[0])
             console.log(file)
             image.innerHTML = `<img src='${file.thumbnail.path}.${file.thumbnail.extension}'>`
-            data.innerHTML = `<h1>${file.name}</h1><p>Series: ${file.series.items[0].name}</p><p>Stories: ${file.stories.items[0].name} </p><p>Description: ${file.description}</p>`
+            data.innerHTML = `<h2>${file.name}</h2><p><span>Series</span> ${file.series.items[0].name}</p><p><span>Stories</span> ${file.stories.items[0].name} </p><p><span>History</span> ${file.description}</p>`
         })
 
 }
@@ -23,8 +30,7 @@ const getSuperhero = (name) => {
 document.getElementById('forms').onclick = (e) => {
     e.preventDefault()
     const text = document.getElementById('heroname')
-    const heroName= text.value.split(' ').join('%20')
+    const heroName = text.value.split(' ').join('%20')
     getSuperhero(heroName)
 }
-
 
