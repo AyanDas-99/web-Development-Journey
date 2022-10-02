d13bd5a39e52626e4f2f36f2172a8dbce855c4a6
Ayan Das <ayandas9531@gmail.com>
2022-10-03 05:00:21 +0530

    eva-chatbot added

---
+++ eva-chatbot/app.js
@@ -0,0 +1,63 @@
+const form = document.querySelector('form')
+const messageDisplay = document.querySelector('.message-display')
+const message = document.querySelector('#message')
+const loader = document.querySelector('.loading')
+const container = document.querySelector('.container')
+
+loading();
+
+function loading() {
+    setTimeout(function(){
+        loader.style.display = 'none'
+        container.style.display = 'block'
+    }, 3000)
+}
+
+
+
+
+form.addEventListener('submit', sendMessage)
+
+function sendMessage(e) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'e0b6052bacmshcfe5da2fca3f2e5p14d484jsn5c655e5224c5',
+            'X-RapidAPI-Host': 'ai-chatbot.p.rapidapi.com'
+        }
+    };
+
+
+    if (message.value !== '') {
+        const msgDiv = document.createElement('div')
+        msgDiv.className = 'user-message'
+        const para = document.createElement('p')
+        para.appendChild(document.createTextNode(message.value))
+        msgDiv.appendChild(para)
+        messageDisplay.appendChild(msgDiv)
+
+        fetch(`https://ai-chatbot.p.rapidapi.com/chat/free?message=${message.value}&uid=user1`, options)
+            .then(response => response.json())
+            .then(response => showResponse(response))
+            .catch(err => console.error(err));
+
+
+
+        messageDisplay.scrollTop = messageDisplay.scrollHeight - messageDisplay.clientHeight;
+        message.value = ''
+        e.preventDefault()
+    }
+
+}
+
+function showResponse(response) {
+    console.log(response)
+    const message = response.chatbot.response
+    const responseDiv = document.createElement('div')
+    responseDiv.className = 'response'
+    const para = document.createElement('p')
+    para.appendChild(document.createTextNode(message))
+    responseDiv.appendChild(para)
+    messageDisplay.appendChild(responseDiv)
+    // messageDisplay.scrollTop = messageDisplay.scrollHeight - messageDisplay.clientHeight;
+}
