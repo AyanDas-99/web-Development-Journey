9617668c4bfce55f17231d81c5f083e8d0602ea9
Ayan Das <ayandas9531@gmail.com>
2022-10-03 14:22:42 +0530

    completed eva-chatbot

--- eva-chatbot/app.js
+++ eva-chatbot/app.js
@@ -7,7 +7,7 @@ const container = document.querySelector('.container')
 loading();
 
 function loading() {
-    setTimeout(function(){
+    setTimeout(function () {
         loader.style.display = 'none'
         container.style.display = 'block'
     }, 3000)
@@ -19,13 +19,24 @@ function loading() {
 form.addEventListener('submit', sendMessage)
 
 function sendMessage(e) {
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': 'e0b6052bacmshcfe5da2fca3f2e5p14d484jsn5c655e5224c5',
+    //         'X-RapidAPI-Host': 'ai-chatbot.p.rapidapi.com'
+    //     }
+    // };
+
+
+    // new one
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'e0b6052bacmshcfe5da2fca3f2e5p14d484jsn5c655e5224c5',
-            'X-RapidAPI-Host': 'ai-chatbot.p.rapidapi.com'
+            'X-RapidAPI-Host': 'aeona3.p.rapidapi.com'
         }
     };
+
 
 
     if (message.value !== '') {
@@ -36,11 +47,15 @@ function sendMessage(e) {
         msgDiv.appendChild(para)
         messageDisplay.appendChild(msgDiv)
 
-        fetch(`https://ai-chatbot.p.rapidapi.com/chat/free?message=${message.value}&uid=user1`, options)
-            .then(response => response.json())
+        // fetch(`https://ai-chatbot.p.rapidapi.com/chat/free?message=${message.value}&uid=user1`, options)
+        //     .then(response => response.json())
+        //     .catch(err => console.error(err));
+
+        // new one
+        fetch(`https://aeona3.p.rapidapi.com/?text=${message.value}&userId=12312312312`, options)
+            .then(response => response.text())
             .then(response => showResponse(response))
             .catch(err => console.error(err));
-
 
 
         messageDisplay.scrollTop = messageDisplay.scrollHeight - messageDisplay.clientHeight;
@@ -51,13 +66,13 @@ function sendMessage(e) {
 }
 
 function showResponse(response) {
-    console.log(response)
-    const message = response.chatbot.response
+    const message = response
+    console.log(message)
     const responseDiv = document.createElement('div')
     responseDiv.className = 'response'
     const para = document.createElement('p')
     para.appendChild(document.createTextNode(message))
     responseDiv.appendChild(para)
     messageDisplay.appendChild(responseDiv)
-    // messageDisplay.scrollTop = messageDisplay.scrollHeight - messageDisplay.clientHeight;
+    messageDisplay.scrollTop = messageDisplay.scrollHeight - messageDisplay.clientHeight;
 }
