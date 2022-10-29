74df0ddfee0e2a75349fb90bc803c0e10a65241c
Ayan Das <ayandas9531@gmail.com>
2022-10-29 19:36:32 +0530

    Promise API

--- JavaScript-course/Promises/app.js
+++ JavaScript-course/Promises/app.js
@@ -33,6 +33,8 @@
 //     console.log(error, 'Failure');
 // })
 
+// loadScript function
+
 const loadScript = (src)=>{
     return new Promise((resolve, reject)=>{
         const script = document.createElement('script');
@@ -47,12 +49,76 @@ const loadScript = (src)=>{
     })
 }
 
-let p1 = loadScript("https://www.google.asdfasdf.js");
+// let p1 = loadScript("https://cdnn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js");
 
-p1.then((value)=>{
-    console.log('value');
-    console.log('Loaded the script');
-}).catch((error)=>{
-    console.log(error);
-    console.log('Problem loading the script');
+// p1.then((value)=>{
+//     console.log(value);
+//     console.log('Loaded the script');
+//     return loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js")
+// }).then((value)=>{
+    
+//     console.log('second script loaded');
+// }).catch((error)=>{
+//     console.log(error);
+//     console.log('Problem loading the script');
+//     console.log(p1)
+// })
+// console.log(p1)
+// Attaching multlple handlers
+
+// let p1 = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         resolve("p1 resolved");
+//     }, 2000)
+// })
+
+// p1.then((value)=>{
+//     const text = document.createElement('h2');
+//     text.textContent = value;
+//     document.body.appendChild(text);
+// })
+
+// p1.then((value)=>{
+//     const para = document.createElement('p');
+//     para.textContent = value;
+//     document.body.appendChild(para);
+// })
+
+// Promise API
+
+
+let p1 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve('This is p1');
+    }, 1000)
+})
+
+let p2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve('This is p2');
+    }, 2000)
+})
+
+let p3 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject('This is p3');
+    }, 3000)
+})
+
+// promise.all
+// let promiseAll = Promise.all([p1, p2, p3]);
+
+// promiseAll.then((value)=>{
+//     console.log(value)
+// })
+
+// promiseAll.catch((value)=>{
+//     console.log(value)
+// })
+
+
+let promiseAll = Promise.allSettled([p1,p2,p3]);
+
+promiseAll.then((value)=>{
+    console.log(value)
 })
