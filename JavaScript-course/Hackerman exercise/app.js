cdc7cd016792f307d48b0b30d6b707147791af98
Ayan Das <ayandas9531@gmail.com>
2022-11-03 20:51:10 +0530

    added: input variables:

---
+++ JavaScript-course/Hackerman exercise/app.js
@@ -0,0 +1,5 @@
+const target = document.querySelector('#name');
+const id = document.querySelector('#ID');
+const submit = document.querySelector('button["type:submit"]');
+
+console.log(target, id, submit)
