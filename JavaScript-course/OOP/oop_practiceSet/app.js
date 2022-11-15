646cb97e5771b6d765e5e2a2c3698a98aba97ffe
Ayan Das <ayandas9531@gmail.com>
2022-11-15 18:13:59 +0530

    added: getters and setters for Complex class

--- JavaScript-course/OOP/oop_practiceSet/app.js
+++ JavaScript-course/OOP/oop_practiceSet/app.js
@@ -1,43 +1,61 @@
-// class Complex{
-//     constructor(real, complex){
-//         this.realPart = real;
-//         this.complexPart = complex;
+class Complex{
+    constructor(real, complex){
+        this.realPart = real;
+        this.complexPart = complex;
+    }
+    theValue(){
+        return `${this.realPart} + ${this.complexPart}i`;
+    }
+    static addComplex(num1, num2){
+        return `${num1.realPart+num2.realPart} + ${num1.complexPart+num2.complexPart}i`
+    }
+    get realPart(){
+        return this._realPart;
+    }
+    get complexPart(){
+        return this._complexPart;
+    }
+    set realPart(real){
+        this._realPart = real;
+    }
+    set complexPart(imaginary){
+        this._complexPart = imaginary;
+    }
+}
+
+const x = new Complex(10, 32);
+const y = new Complex(13, 23);
+
+console.log(Complex.addComplex(x, y));
+console.log(x.realPart)
+x.realPart = 13;
+console.log(Complex.addComplex(x, y));
+
+
+
+
+// class Human {
+//     constructor(name, gender, age){
+//         this.name = name;
+//         this.gender = gender;
+//         this.age = age;
 //     }
-//     theValue(){
-//         return `${this.realPart} + ${this.complexPart}i`;
-//     }
-//     static addComplex(num1, num2){
-//         return `${num1.realPart+num2.realPart} + ${num1.complexPart+num2.complexPart}i`
+//     introduce(){
+//         console.log(`Hi, I'm ${this.name}. I am ${this.age} years old.`);
 //     }
 // }
 
-// const x = new Complex(10, 32);
-// const y = new Complex(13, 23);
-
-// console.log(Complex.addComplex(x, y));
-
-class Human {
-    constructor(name, gender, age){
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-    }
-    introduce(){
-        console.log(`Hi, I'm ${this.name}. I am ${this.age} years old.`);
-    }
-}
-
-class Student extends Human{
-    constructor(name, gender, age, org){
-        super(name, gender, age);
-        this.organization = org;
-    }
-    introduce(){
-        super.introduce();
-        console.log(`I am also a student of ${this.organization}`);
-    }
-}
+// class Student extends Human{
+//     constructor(name, gender, age, org){
+//         super(name, gender, age);
+//         this.organization = org;
+//     }
+//     introduce(){
+//         super.introduce();
+//         console.log(`I am also a student of ${this.organization}`);
+//     }
+// }
 
 
-const me = new Student('Ayan Das', 'Male', 19, 'JNRM');
-me.introduce();
+// const me = new Student('Ayan Das', 'Male', 19, 'JNRM');
+// me.introduce();
